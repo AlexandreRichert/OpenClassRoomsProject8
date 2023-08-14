bc6fb3bd775705e5224d653108fa75c96e076c69
Alexandre Richert <121611457+AlexandreRichert@users.noreply.github.com>
2023-08-14 23:09:42 +0200

    responsive accueil ok

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,22 +11,22 @@ function Main() {
   };
   
   return (
-    <div id='main' className="  w-full h-screen pb-20 flex flex-col-reverse justify-center items-center bg-cover bg-no-repeat gap-6 " style={backgroundStyle}>
+    <div id='main' className="w-full h-screen pb-20 flex flex-col-reverse justify-center items-center bg-cover bg-no-repeat gap-6 " style={backgroundStyle}>
       <Link to="portfolio" smooth={true} duration={500}>
-        <button className='my-7 w-12 h-8 rounded-full shadow-2xl bg-blue-300 hover:bg-[#EDB88B] ease-in-out duration-300'>
-          <AiFillDownCircle className='mx-auto'/>
+        <button className='my-7 w-12 h-8 sm:w-20 sm:h-12 rounded-full shadow-2xl bg-blue-300 hover:bg-[#EDB88B] ease-in-out duration-300'>
+          <AiFillDownCircle className='mx-auto sm:text-4xl'/>
         </button>
       </Link>
-      <div className='flex flex-col justify-center items-center'>
-        <h1 className='text-4xl sm:text-7xl font-bold text-white'>
+      <div className='flex flex-col justify-center items-center gap-3'>
+        <h1 className='text-4xl sm:text-7xl sm:mt-6 font-bold text-white'>
           RICHERT Alexandre
         </h1>
-        <h2 className='text-4xl sm:text-7xl  text-center font-bold text-[#EDB88B]'>
+        <h2 className='text-3xl sm:text-6xl  text-center font-bold text-[#EDB88B]'>
           Développeur web junior
         </h2>
       </div>
       <div>
-        <img src={photo} alt="Développement front-end" className='w-52 h-auto rounded-full bg-[#075C74]' />
+        <img src={photo} alt="Développement front-end" className='w-52 sm:w-96 h-auto rounded-full bg-[#075C74]' />
       </div>
 
     </div>
